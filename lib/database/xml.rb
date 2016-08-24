6811c6a767cd67309ae6ae313eb8ba4708e843e5
shitake <z1522716486@hotmail.com>
2016-08-24 14:06:29 +0800

    update xml.rb

--- lib/database/xml.rb
+++ lib/database/xml.rb
@@ -1,6 +1,8 @@
-require 'pp'
+#encoding: utf-8
+#author: shitake
+#data: 16-8-24
 
-class Parser
+class XMLLikeParser
 
   class Node
     attr_accessor :name
@@ -23,17 +25,12 @@ class Parser
     end
   end
 
+  attr_reader :root
+
   def initialize(code, conf = {})
-    @node_style = conf[:node_style]
-    @attr_style = conf[:attr_style]
-    @end_node = conf[:end_node]
-    @nodes = conf[:nodes]
+    @node_style, @attr_style, @end_node = conf
     @root = Node.new('root', {}, code)
     parse(@root)
-  end
-
-  def root
-    @root
   end
 
   def parse(node)
@@ -48,84 +45,59 @@ class Parser
     return child unless code
     code.strip!
     str = ''
-    if @node_style.class == Array
-      @node_style.each do |s|
-        str = code[s]
-        break if str
-      end
+    index = 0
+    new_node = nil
+    if code == ''
+      return child
+    elsif code[0] != '<'
+      new_node = Node.new('text', {})
+      new_node.value = code.include?('<') ? code[0, index = code.index('<')] : (index = code.size;code)
+      child.push(new_node)
     else
-      str = code[@node_style]
-    end
-    unless str || (@nodes ? !@nodes.include?($1) : false)
-      if child[-1] && child[-1].name == 'text'
-        child[-1].value += code
-      elsif code != ''
+      str = (@node_style.class == Array) ? @node_style.each{ |node| break code[node] if code[node] } : code[@node_style]
+      if str
+        index = code.index(str) + str.size
+        new_node = Node.new($1, parse_attr($2, {}), $3)
+        child.push(new_node)
+      else
+        index = code.size
         new_node = Node.new('text', {})
         new_node.value = code
         child.push(new_node)
       end
-      return child
     end
-    new_node = Node.new($1, parse_attr($2, {}), $3)
-    index = code.index(str) + str.length
-    code = code[index, code.length - index]
-    child.push(new_node)
-    parse_node(code, child)
+    parse_node(code[index, code.size], child) if code.size > index
+    child
   end
 
   def parse_attr(code ,attr)
-    return {} unless code
-    str = ''
-    if @attr_style.class == Array
-      @attr_style.each do |s|
-        str = code[s]
-        break if str
-      end
-    else
-      str = code[@attr_style]
-    end
+    return attr unless code
+    code.strip!
+    str = code[@attr_style]
     return attr unless str
-    index = code.index(str) + str.length
-    code = code[index, code.length - index]
+    index = code.index(str) + str.size
+    code = code[index, code.size - index]
     attr[$1.to_sym] = $2
     parse_attr(code, attr)
   end
 
 end
 
-
-xcode = {
-    :node_style => /\[([a-zA-z0-9]*)( [a-zA-z0-9]*=\".*\")*\](.*)\[\/\1\]/,
-    :attr_style => /([a-zA-Z0-9]*)=\"([^\"]*)\"/,
-    :end_node => ['text'],
-    :nodes => nil
-}
-
-# ====================================================================================
-
 module XML
   class <<self
-    def read(path)
-      open(path,'r') do |f|
-        return xml = f.read
-      end
+    def xml2node(code)
+      xml = [
+          [/^<([a-zA-Z]+[^<^ ]*)([^<]*)\/>/, /^<([a-zA-Z]+[^<^ ]*)([^<]*)>(.*)<\/\1>/m],
+          /([a-zA-Z]+[^=]*)="([^"]*)"/,
+          ['text']
+      ]
+      XMLLikeParser.new(code, xml).root
     end
-
-    def xml2node(path)
-      code = path#read(path)
-      xml = {
-          :node_style => [/<([a-zA-Z]+[^<^ ]*)([^<]*)>(.*)<\/\1>/, /<([a-zA-Z]+[^<^ ]*)([^<]*)\/>/],#|<([a-zA-z0-9]*)( [a-zA-z0-9]*=\".*\")*[ ]*\/>/,#[/<([a-zA-z0-9]*)( [a-zA-z0-9]*=\".*\")*>(.*)<\/\1>/, /<([a-zA-z0-9]*)( [a-zA-z0-9]*=\".*\")*[ ]*\/>/],
-          :attr_style => /[ ]*([a-zA-Z]+[^=]*)="([^"]*)"/,
-          :end_node => ['text'],
-          :nodes => nil
-      }
-      Parser.new(code, xml)
-    end
-
   end
 end
 
-xml = '<s:button><pos x="12" y="23">xssss<type s="23232"/></pos></s:button>'
-
-
-XML.xml2node(xml).root.child.each{|n| pp n}
+module Kernel
+  def xml(value)
+    XML.xml2node(value)
+  end
+end
